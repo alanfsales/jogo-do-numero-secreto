bf88a3e7e11abbbd62b80f276894af62439898aa
AlanFSales <alanfsales27@gmail.com>
2024-02-05 16:28:11 -0300

    Alterar o número máximo sorteado

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag, texto){
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1','Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número de 1 a 10');   
+    exibirTextoNaTela('p', 'Escolha um número de 1 a 100');   
 }
 
 exibirMensagemInicial();
